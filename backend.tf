d27703ebe75d3e9cbc7905d64219bfc6669e29aa
Shreyastc1 <135942784+Shreyastc1@users.noreply.github.com>
2024-02-08 16:23:00 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "do-not-delete-shreyas-terraform-backend"                   
-    key    = "backend/shreyas/terraform.tfstate"
-    region = "ap-south-1"                  #replce the region
-    dynamodb_table = "terraform-state-lock-DO-NOT-DELETE-shreyas"          
-}
-}
